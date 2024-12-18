ccfa552379f480363701fd31897403f68df1f32d
吉本みゆき <watagashi1213@gmail.com>
2024-12-18 14:00:38 +0900

    add:admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_18_040328) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_18_042254) do
+  create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "happies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
