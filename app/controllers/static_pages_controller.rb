083d830042249bbc0e2fd7a74d1fb2db6e0bd688
吉本みゆき <watagashi1213@gmail.com>
2024-12-18 14:53:51 +0900

    update:topのビュー

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,22 +2,22 @@ class StaticPagesController < ApplicationController
   def top; end
 
   def show
-    @random_word = Happy.order("RANDOM()").first&.text
+    @random_word = Happy.order("RAND()").first&.text
   end
 
   def random_word
-    @random_word = Happy.order("RANDOM()").first&.text
+    @random_word = Happy.order("RAND()").first&.text
     respond_to do |format|
       format.js
     end
   end
 
   def unhappy
-    @unhappy_word = Unhappy.order("RANDOM()").first&.text
+    @unhappy_word = Unhappy.order("RAND()").first&.text
   end
 
   def random_unhappy_word
-    @unhappy_word = Unhappy.order("RANDOM()").first&.text
+    @unhappy_word = Unhappy.order("RAND()").first&.text
     respond_to do |format|
       format.js
     end
